ecb34f5fbbb025f98d5af8529a46099c0bf0535b
David Wischnjak <davidw.software@gmail.com>
2023-01-08 23:53:34 +0100

    fix pin

--- examples/Listen_and_print/Listen_and_print.ino
+++ examples/Listen_and_print/Listen_and_print.ino
@@ -5,10 +5,10 @@ const int SPI_SCK = 18; 			//board or mcu specific
 const int SPI_MISO = 19; 			//board or mcu specific
 const int SPI_MOSI = 23; 			//board or mcu specific
 const int SPI_CS = 5; 				//select any pin
-const int RADIO_INPUT_PIN = 13; 	//select any pin
-const int RADIO_OUTPUT_PIN = 12; 	//select any pin
+const int RADIO_INPUT_PIN = 12; 	//select any pin, this is the TX-PIN
+const int RADIO_OUTPUT_PIN = 13; 	//select any pin, this is the RX-PIN
 
-CC1101 cc1101(SPI_SCK, SPI_MISO, SPI_MOSI, SPI_CS, RADIO_OUTPUT_PIN, RADIO_INPUT_PIN);
+CC1101 cc1101(SPI_SCK, SPI_MISO, SPI_MOSI, SPI_CS, RADIO_INPUT_PIN, RADIO_OUTPUT_PIN);
 
 volatile long last_micros;
 String buffer;
@@ -16,7 +16,7 @@ String buffer;
 void radioHandlerOnChange() {
 	int delta_micros = micros() - last_micros;
 	
-	bool input = digitalRead(RADIO_INPUT_PIN);
+	bool input = digitalRead(RADIO_OUTPUT_PIN);
 	if(input == 1){
 		buffer += "\n0 -> 1 after " + String(delta_micros);
 	} else {
@@ -30,12 +30,15 @@ void setup(){
 	Serial.begin(115200);
 	
 	cc1101.init();
-	cc1101.setMHZ(433.9);
+	printfDebug("CC1101: 0x%02x, version: 0x%02x\n", cc1101.getPartnum(), cc1101.getVersion());
+	cc1101.setMHZ(433.92);
+	cc1101.setTXPwr(TX_PLUS_10_DBM);
 	cc1101.setDataRate(5000);
+	cc1101.setModulation(ASK_OOK);
 
 	cc1101.setRx();
 
-	attachInterrupt(RADIO_INPUT_PIN, radioHandlerOnChange, CHANGE);
+	attachInterrupt(RADIO_OUTPUT_PIN, radioHandlerOnChange, CHANGE);
 }
 
 volatile long last_millis;
